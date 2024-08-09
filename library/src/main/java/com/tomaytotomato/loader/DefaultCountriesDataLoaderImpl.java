1b86acd1c82f50f95025977a4b7cc85f7cb24998
tomaytotomato <971697+tomaytotomato@users.noreply.github.com>
2024-08-09 14:00:26 +0100

    2 remove the need for Jackson in the library
    Created a tool module that converts the JSON dataset into a binary format of the location4j classes (Country, State, City, Timezone)
    
    Some improvements:
    
    Test run before: 222ms
    Test run after: 190ms
    
    Test search before: 194ms
    Test search after: 165ms
    
    Disk before: 35.1 mb
    Disk after: 21.2mb

---
+++ library/src/main/java/com/tomaytotomato/loader/DefaultCountriesDataLoaderImpl.java
@@ -0,0 +1,50 @@
+package com.tomaytotomato.loader;
+
+import com.tomaytotomato.model.Country;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DefaultCountriesDataLoaderImpl implements CountriesDataLoader {
+
+  private static final String DEFAULT_FILE = "location4j.bin";
+  private final List<Country> countries = new ArrayList<>();
+
+  /**
+   * Loads a list of {@link Country} from the location4j.bin file.
+   */
+  public DefaultCountriesDataLoaderImpl() {
+    var logger = Logger.getLogger(this.getClass().getName());
+    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(DEFAULT_FILE)) {
+      logger.info("Attempting to load countries from " + DEFAULT_FILE);
+      if (inputStream == null) {
+        throw new IllegalArgumentException("File not found: " + DEFAULT_FILE);
+      }
+
+      loadLocationsFromBinary(inputStream, logger);
+    } catch (IOException | ClassNotFoundException e) {
+      logger.log(Level.SEVERE, "Failed to load countries file: " + e.getMessage(), e);
+    }
+  }
+
+  private void loadLocationsFromBinary(InputStream inputStream, Logger logger)
+      throws IOException, ClassNotFoundException {
+    try (var objectInputStream = new ObjectInputStream(inputStream)) {
+      // Read the list of countries from the binary file
+      List<Country> loadedCountries = (List<Country>) objectInputStream.readObject();
+      this.countries.addAll(loadedCountries);
+      logger.info("Successfully loaded countries binary file");
+    } catch (IOException | ClassNotFoundException e) {
+      logger.log(Level.SEVERE, "Failed to parse countries file: " + e.getMessage(), e);
+    }
+  }
+
+  @Override
+  public List<Country> getCountries() {
+    return countries;
+  }
+}
---
+++ library/src/main/java/com/tomaytotomato/model/Location.java
@@ -0,0 +1,223 @@
+package com.tomaytotomato.model;
+
+import java.math.BigDecimal;
+
+public class Location {
+
+  private String countryName;
+  private Integer countryId;
+  private String countryIso2Code;
+  private String countryIso3Code;
+  private String state;
+  private Integer stateId;
+  private String stateCode;
+  private String stateName;
+  private String city;
+  private Integer cityId;
+  private BigDecimal latitude;
+  private BigDecimal longitude;
+
+  public Location() {
+  }
+
+  public Location(String countryName, Integer countryId, String countryIso2Code,
+      String countryIso3Code, String state, Integer stateId, String stateCode, String stateName, String city,
+      Integer cityId, BigDecimal latitude, BigDecimal longitude) {
+    this.countryName = countryName;
+    this.countryId = countryId;
+    this.countryIso2Code = countryIso2Code;
+    this.countryIso3Code = countryIso3Code;
+    this.state = state;
+    this.stateId = stateId;
+    this.stateCode = stateCode;
+    this.stateName = stateName;
+    this.city = city;
+    this.cityId = cityId;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  public String getCountryName() {
+    return countryName;
+  }
+
+  public void setCountryName(String countryName) {
+    this.countryName = countryName;
+  }
+
+  public Integer getCountryId() {
+    return countryId;
+  }
+
+  public void setCountryId(Integer countryId) {
+    this.countryId = countryId;
+  }
+
+  public String getCountryIso2Code() {
+    return countryIso2Code;
+  }
+
+  public void setCountryIso2Code(String countryIso2Code) {
+    this.countryIso2Code = countryIso2Code;
+  }
+
+  public String getCountryIso3Code() {
+    return countryIso3Code;
+  }
+
+  public void setCountryIso3Code(String countryIso3Code) {
+    this.countryIso3Code = countryIso3Code;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public Integer getStateId() {
+    return stateId;
+  }
+
+  public void setStateId(Integer stateId) {
+    this.stateId = stateId;
+  }
+
+  public String getStateCode() {
+    return stateCode;
+  }
+
+  public void setStateCode(String stateCode) {
+    this.stateCode = stateCode;
+  }
+
+  public String getStateName() {
+    return stateName;
+  }
+
+  public void setStateName(String stateName) {
+    this.stateName = stateName;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public Integer getCityId() {
+    return cityId;
+  }
+
+  public void setCityId(Integer cityId) {
+    this.cityId = cityId;
+  }
+
+  public BigDecimal getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(BigDecimal latitude) {
+    this.latitude = latitude;
+  }
+
+  public BigDecimal getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(BigDecimal longitude) {
+    this.longitude = longitude;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+
+    private String countryName;
+    private Integer countryId;
+    private String countryIso2Code;
+    private String countryIso3Code;
+    private String state;
+    private Integer stateId;
+    private String stateCode;
+    private String stateName;
+    private String city;
+    private Integer cityId;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    Builder() { }
+
+    public Builder countryName(String countryName) {
+      this.countryName = countryName;
+      return this;
+    }
+
+    public Builder countryId(Integer countryId) {
+      this.countryId = countryId;
+      return this;
+    }
+
+    public Builder countryIso2Code(String countryIso2Code) {
+      this.countryIso2Code = countryIso2Code;
+      return this;
+    }
+
+    public Builder countryIso3Code(String countryIso3Code) {
+      this.countryIso3Code = countryIso3Code;
+      return this;
+    }
+
+    public Builder state(String state) {
+      this.state = state;
+      return this;
+    }
+
+    public Builder stateId(Integer stateId) {
+      this.stateId = stateId;
+      return this;
+    }
+
+    public Builder stateCode(String stateCode) {
+      this.stateCode = stateCode;
+      return this;
+    }
+
+    public Builder stateName(String stateName) {
+      this.stateName = stateName;
+      return this;
+    }
+
+    public Builder city(String city) {
+      this.city = city;
+      return this;
+    }
+
+    public Builder cityId(Integer cityId) {
+      this.cityId = cityId;
+      return this;
+    }
+
+    public Builder latitude(BigDecimal latitude) {
+      this.latitude = latitude;
+      return this;
+    }
+
+    public Builder longitude(BigDecimal longitude) {
+      this.longitude = longitude;
+      return this;
+    }
+
+    public Location build() {
+      return new Location(countryName, countryId, countryIso2Code, countryIso3Code, state, stateId,
+          stateCode, stateName, city, cityId, latitude, longitude);
+    }
+
+  }
+}
