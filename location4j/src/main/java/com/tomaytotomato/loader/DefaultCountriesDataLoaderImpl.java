ed00259a01aa02d112a2d1a7ff07f5b1f0c951b7
Joseph Ottinger <joeo@enigmastation.com>
2024-08-10 08:21:16 -0400

    1. Add spotless to validate formatting and allow consistency 2. Add check for location4j.bin such that it's from the same classloader as the default loader 3. Add lat/long search for city
    Closes #17
    Closes #18
    Closes #19

--- location4j/src/main/java/com/tomaytotomato/loader/DefaultCountriesDataLoaderImpl.java
+++ location4j/src/main/java/com/tomaytotomato/loader/DefaultCountriesDataLoaderImpl.java
@@ -14,39 +14,49 @@ import java.util.logging.Logger;
  */
 public class DefaultCountriesDataLoaderImpl implements CountriesDataLoader {
 
-  private static final String DEFAULT_FILE = "/location4j.bin";
-  private final List<Country> countries = new ArrayList<>();
+    private static final String DEFAULT_FILE = "/location4j.bin";
+    private final List<Country> countries = new ArrayList<>();
 
-  /**
-   * Loads a list of {@link Country} from the location4j.bin file.
-   */
-  public DefaultCountriesDataLoaderImpl() {
-    var logger = Logger.getLogger(this.getClass().getName());
-    try (InputStream inputStream = getClass().getResourceAsStream(DEFAULT_FILE)) {
-      logger.info("Attempting to load countries from " + DEFAULT_FILE);
-      if (inputStream == null) {
-        throw new IllegalArgumentException("File not found: " + DEFAULT_FILE);
-      }
+    /**
+     * Loads a list of {@link Country} from the location4j.bin file.
+     */
+    public DefaultCountriesDataLoaderImpl() {
+        var logger = Logger.getLogger(this.getClass().getName());
 
-      loadLocationsFromBinary(inputStream, logger);
-    } catch (IOException | ClassNotFoundException e) {
-      logger.log(Level.SEVERE, "Failed to load countries file: " + e.getMessage(), e);
+        String urlToFile = getClass().getResource(DEFAULT_FILE).toString();
+        String urlToThis =
+                this.getClass().getResource(this.getClass().getSimpleName() + ".class").toString();
+        String trimmed = urlToFile.substring(0, urlToFile.indexOf(DEFAULT_FILE));
+        if (!urlToThis.substring(0, trimmed.length()).equals(trimmed)) {
+            throw new RuntimeException(
+                    DEFAULT_FILE + " is not in the same artifact as the loader: security issue");
+        }
+
+        try (InputStream inputStream = getClass().getResourceAsStream(DEFAULT_FILE)) {
+            logger.info("Attempting to load countries from " + DEFAULT_FILE);
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: " + DEFAULT_FILE);
+            }
+
+            loadLocationsFromBinary(inputStream, logger);
+        } catch (IOException | ClassNotFoundException e) {
+            logger.log(Level.SEVERE, "Failed to load countries file: " + e.getMessage(), e);
+        }
     }
-  }
 
-  private void loadLocationsFromBinary(InputStream inputStream, Logger logger)
-      throws IOException, ClassNotFoundException {
-    try (var objectInputStream = new ObjectInputStream(inputStream)) {
-      List<Country> loadedCountries = (List<Country>) objectInputStream.readObject();
-      this.countries.addAll(loadedCountries);
-      logger.info("Successfully loaded countries binary file");
-    } catch (IOException | ClassNotFoundException e) {
-      logger.log(Level.SEVERE, "Failed to parse countries file: " + e.getMessage(), e);
+    private void loadLocationsFromBinary(InputStream inputStream, Logger logger)
+            throws IOException, ClassNotFoundException {
+        try (var objectInputStream = new ObjectInputStream(inputStream)) {
+            List<Country> loadedCountries = (List<Country>) objectInputStream.readObject();
+            this.countries.addAll(loadedCountries);
+            logger.info("Successfully loaded countries binary file");
+        } catch (IOException | ClassNotFoundException e) {
+            logger.log(Level.SEVERE, "Failed to parse countries file: " + e.getMessage(), e);
+        }
     }
-  }
 
-  @Override
-  public List<Country> getCountries() {
-    return countries;
-  }
+    @Override
+    public List<Country> getCountries() {
+        return countries;
+    }
 }
--- location4j/src/test/java/com/tomaytotomato/usecase/FindCityTest.java
+++ location4j/src/test/java/com/tomaytotomato/usecase/FindCityTest.java
@@ -1,12 +1,13 @@
 package com.tomaytotomato.usecase;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.tomaytotomato.LocationService;
 import com.tomaytotomato.loader.DefaultCountriesDataLoaderImpl;
 import com.tomaytotomato.text.normaliser.DefaultTextNormaliser;
+import java.math.BigDecimal;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,96 +18,113 @@ import org.junit.jupiter.params.provider.CsvSource;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FindCityTest {
 
-  private final FindCity locationService;
+    private final FindCity locationService;
 
-  public FindCityTest() {
-    var textNormaliser = new DefaultTextNormaliser();
-    var dataLoader = new DefaultCountriesDataLoaderImpl();
-    locationService = new LocationService(textNormaliser, dataLoader);
-  }
+    public FindCityTest() {
+        var textNormaliser = new DefaultTextNormaliser();
+        var dataLoader = new DefaultCountriesDataLoaderImpl();
+        locationService = new LocationService(textNormaliser, dataLoader);
+    }
 
-  @Description("Find City By ID, when null then throw exception")
-  @Test
-  void findCityById_WhenNull_ThenThrowException() {
-    // When Then
-    assertThatThrownBy(() -> locationService.findCityById(null))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("City ID cannot be null");
-  }
+    @Description("Find City by Lat/long with BigDecimal")
+    @Test
+    void findCityByLatLongWithBigDecimal() {
+        var result =
+                locationService.findClosestCityByLatLong(
+                        new BigDecimal("30.43826000"), new BigDecimal("-84.28073000"));
+        assertEquals("Tallahassee", result.getName());
+    }
 
-  @Description("Find City By ID, when ID does not exist then return Optional empty")
-  @Test
-  void findCityById_WhenIdDoesNotExist_ThenReturnEmpty() {
-    // When
-    var result = locationService.findCityById(900000);
+    @Description("Find City by Lat/long with double")
+    @Test
+    void findCityByLatLongWithDouble() {
+        var result = locationService.findClosestCityByLatLong(30.438, -84.280);
+        assertEquals("Tallahassee", result.getName());
+    }
 
-    // Then
-    assertThat(result).isEmpty();
-  }
+    @Description("Find City By ID, when null then throw exception")
+    @Test
+    void findCityById_WhenNull_ThenThrowException() {
+        // When Then
+        assertThatThrownBy(() -> locationService.findCityById(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("City ID cannot be null");
+    }
 
-  @Description("Find City By ID, when ID exists then return city")
-  @ParameterizedTest
-  @CsvSource({
-      "1, Andorra la Vella",
-      "24601, Dassow",
-      "26, Bani Yas City",
-      "1652, Amlach",
-  })
-  void findCityById_WhenIdExists_ThenReturnCity(Integer id, String expectedCityName) {
+    @Description("Find City By ID, when ID does not exist then return Optional empty")
+    @Test
+    void findCityById_WhenIdDoesNotExist_ThenReturnEmpty() {
+        // When
+        var result = locationService.findCityById(900000);
 
-    // When
-    var result = locationService.findCityById(id);
+        // Then
+        assertThat(result).isEmpty();
+    }
 
-    // Then
-    assertThat(result).isNotEmpty().get().extracting("name").isEqualTo(expectedCityName);
-  }
+    @Description("Find City By ID, when ID exists then return city")
+    @ParameterizedTest
+    @CsvSource({
+        "1, Andorra la Vella",
+        "24601, Dassow",
+        "26, Bani Yas City",
+        "1652, Amlach",
+    })
+    void findCityById_WhenIdExists_ThenReturnCity(Integer id, String expectedCityName) {
 
-  @Description("Find All Cities, when called is not empty")
-  @Test
-  void findAllCities_WhenCalled_ThenReturnListOfCities() {
+        // When
+        var result = locationService.findCityById(id);
 
-    // When
-    var result = locationService.findAllCities();
+        // Then
+        assertThat(result).isNotEmpty().get().extracting("name").isEqualTo(expectedCityName);
+    }
 
-    // Then
-    assertThat(result).isNotEmpty().hasSize(150661);
-  }
+    @Description("Find All Cities, when called is not empty")
+    @Test
+    void findAllCities_WhenCalled_ThenReturnListOfCities() {
 
-  @Description("Find All Cities By City Name, when city name null or blank then throw exception")
-  @Test
-  void findAllCitiesByCityName_WhenStateNameIsNullOrBlank_ThenThrowException() {
-    // When Then
-    assertThatThrownBy(() -> locationService.findAllCitiesByCityName(null))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("City Name cannot be null or empty");
-  }
+        // When
+        var result = locationService.findAllCities();
 
-  @Description("Find All Cities By City Name, when city name not found then return empty list")
-  @Test
-  void findAllCitiesByCityName_WhenCityNameDoesNotExist_ThenReturnEmptyList() {
-    // When
-    var result = locationService.findAllCitiesByCityName("Raccoon City");
+        // Then
+        assertThat(result).isNotEmpty().hasSize(150661);
+    }
 
-    // Then
-    assertThat(result).isEmpty();
-  }
+    @Description("Find All Cities By City Name, when city name null or blank then throw exception")
+    @Test
+    void findAllCitiesByCityName_WhenStateNameIsNullOrBlank_ThenThrowException() {
+        // When Then
+        assertThatThrownBy(() -> locationService.findAllCitiesByCityName(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("City Name cannot be null or empty");
+    }
 
-  @DisplayName("Find All Cities By City Name, when city name is found then return list of states")
-  @ParameterizedTest
-  @CsvSource({
-      "Glasgow, 6",
-      "London,  6",
-      "Delhi, 4",
-      "Beijing,  1",
-      "Manchester,  16",
-      "San Francisco, 30"
-  })
-  void findAllCitiesByCityName_WhenStateNameExists_ThenReturnStates(String cityName, Integer expectedCount) {
+    @Description("Find All Cities By City Name, when city name not found then return empty list")
+    @Test
+    void findAllCitiesByCityName_WhenCityNameDoesNotExist_ThenReturnEmptyList() {
+        // When
+        var result = locationService.findAllCitiesByCityName("Raccoon City");
 
-    // When
-    var results = locationService.findAllCitiesByCityName(cityName);
+        // Then
+        assertThat(result).isEmpty();
+    }
 
-    // Then
-    assertThat(results).hasSize(expectedCount);
-  }
+    @DisplayName("Find All Cities By City Name, when city name is found then return list of states")
+    @ParameterizedTest
+    @CsvSource({
+        "Glasgow, 6",
+        "London,  6",
+        "Delhi, 4",
+        "Beijing,  1",
+        "Manchester,  16",
+        "San Francisco, 30"
+    })
+    void findAllCitiesByCityName_WhenStateNameExists_ThenReturnStates(
+            String cityName, Integer expectedCount) {
+
+        // When
+        var results = locationService.findAllCitiesByCityName(cityName);
+
+        // Then
+        assertThat(results).hasSize(expectedCount);
+    }
 }
